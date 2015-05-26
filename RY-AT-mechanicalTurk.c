0110114ebd85935f1bf3a9ac0926240a9b3d9506
ruby24yu <artisticice@gmail.com>
2015-05-26 17:13:59 +1000

    Update RY-AT-mechanicalTurk.c

--- RY-AT-mechanicalTurk.c
+++ RY-AT-mechanicalTurk.c
@@ -19,19 +19,43 @@ action decideAction (Game g) {
     pathVertex[2] = "RRL";
 
     int player = getWhoseTurn (g);
+    //get number of resources
     int numMJ = getStudents (g, player, STUDENT_MJ);
     int numMTV = getStudents (g, player, STUDENT_MTV);
     int numMMONEY = getStudents (g, player, STUDENT_MMONEY);
-
+    int numTHD = getStudents(g,player,STUDENT_THD);
+    int numBPS = getStudents(g,player,STUDENT_BPS);
+    int numBQN = getStudents(g,player,STUDENT_BQN);
+    
+    
     action nextAction;
-
+    nextAction.actionCode = PASS;
+    //make arcs
+    if (numBQN >= 1 && numBPS >= 1){
+        int validArcFound = 0;
+        int counter=0;
+        action getArc;
+        getArc.actionCode = OBTAIN_ARC;
+        //while loop to try to find possible path to build arc on
+        while (counter < 72){
+            getArc.destination = allPaths[counter];
+            if (isLegalAction(g, getArc)){
+                validArcFound = counter;
+            }
+            
+            counter++
+        }
+        
+        if(validArcFound){
+            nextAction.actionCode = OBTAIN_ARC;
+            nextAction.destination = allPaths[validArcFound];
+        }
+    }
+    
+    //start spinoff
     if (numMJ > 0 && numMTV > 0 && numMMONEY > 0) {
         nextAction.actionCode = START_SPINOFF;
-    } else {
-        nextAction.actionCode = PASS;
     }
-
-    makeAction (g, nextAction);
 
     return nextAction;
 }
