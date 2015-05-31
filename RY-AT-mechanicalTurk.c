d45985e961fb7a41ace24b5f540168daaf05086e
atrinh4 <atrinh4@users.noreply.github.com>
2015-05-31 11:18:28 +1000

    Update RY-AT-mechanicalTurk.c

--- RY-AT-mechanicalTurk.c
+++ RY-AT-mechanicalTurk.c
@@ -17,10 +17,12 @@
 
 #define NUM_EDGES 72
 
-#define NUM_DISCIPLINES 6
+#define MAX_DISCIPLINE 6
 
-int isActionCostMet (int *numDiscipline, int actionCode);
-action retrainStudents (Game g, int actionCode, int retrainTo, int retrainFrom);
+int isActionCostMet (int actionCode, int *numDiscipline);
+int canRetrain (Game g, int actionCode, int *numDiscipline);
+int determineRetrainTo (int actionCode, int *numDiscipline);
+int determineRetrainFrom (Game g, int actionCode, int *numDiscipline);
 
 action decideAction (Game g) {
 
@@ -35,7 +37,7 @@ action decideAction (Game g) {
     "LRLRRL","LRRLL","LRRL","RLRL","RRLLRLL","RLRLRLR","LRRLRLRL","LRRLRL","LRRLR","RLRLL","RLRLR","RLRLRL",
     "RLRLRLL","LRRLRLR","LRRLRL",
     };
-	
+
 	// arbitrary counters
     int i = 0;
 	int counter = 0;
@@ -58,8 +60,8 @@ action decideAction (Game g) {
 
 	// number of students for each discipline
 	int curDiscipline = 0;
-	int numDiscipline[NUM_DISCIPLINES];
-	while (curDiscipline < NUM_DISCIPLINES) {
+	int numDiscipline[MAX_DISCIPLINE];
+	while (curDiscipline < MAX_DISCIPLINE) {
 		numDiscipline[curDiscipline] = getStudents (g, player, curDiscipline);
 		curDiscipline++;
 	}
@@ -67,13 +69,11 @@ action decideAction (Game g) {
     // default action
     action nextAction;
     nextAction.actionCode = PASS;
-	int retrainFrom = STUDENT_BPS;
-	int retrainTo = STUDENT_BPS;
 
 	if (numCampus > 5) {
 		printf ("\n---In makeGO8()--\n");
 		// make GO8
-		if (isActionCostMet (numDiscipline, BUILD_GO8)){
+		if (isActionCostMet ( BUILD_GO8, numDiscipline)){
 		    //while loop to try to find possible campus to build GO8 on
 			action isValidGO8;
 			isValidGO8.actionCode = BUILD_GO8;
@@ -86,36 +86,19 @@ action decideAction (Game g) {
 		        	i++;
 				}
 		    }
-		} else {
+		} else if (canRetrain (g, BUILD_GO8, numDiscipline)) {
 			action isValidRetrain;
 			isValidRetrain.actionCode = RETRAIN_STUDENTS;
-			retrainTo = STUDENT_MJ;
-			while (retrainTo < NUM_DISCIPLINES) {
-				if ((retrainTo != STUDENT_MTV) && (numDiscipline[retrainTo] < 1)) {
-					while (retrainFrom < NUM_DISCIPLINES) {
-						if (retrainFrom == STUDENT_MJ || retrainFrom == STUDENT_MMONEY) {
-							if (numDiscipline[retrainFrom] >=
-								getExchangeRate (g, player, retrainFrom, retrainTo)+3) {
-								isValidRetrain.disciplineTo = retrainTo;
-								isValidRetrain.disciplineFrom = retrainFrom;
-							}
-						} else {
-							isValidRetrain.disciplineTo = retrainTo;
-							isValidRetrain.disciplineFrom = retrainFrom;
-						}
-						if (isLegalAction (g, isValidRetrain)) {
-							nextAction = isValidRetrain;
-						}
-						retrainFrom++;
-					}
-				}
-				retrainTo++;
+			isValidRetrain.disciplineTo = determineRetrainTo (BUILD_GO8, numDiscipline);
+			isValidRetrain.disciplineFrom = determineRetrainFrom (g, BUILD_GO8, numDiscipline);
+			if (isLegalAction (g, isValidRetrain)) {
+				nextAction = isValidRetrain;
 			}
 		}
 	} else if (numVacantARC == 0) {
 		printf ("\n---In makeCampus()--\n");
 		// make Campuses
-		if (isActionCostMet (numDiscipline, BUILD_CAMPUS)){
+		if (isActionCostMet (BUILD_CAMPUS, numDiscipline)){
 		    //while loop to try to find possible vertex to build campus on
 			action isValidCampus;
 			isValidCampus.actionCode = BUILD_CAMPUS;
@@ -128,36 +111,19 @@ action decideAction (Game g) {
 		        	i++;
 				}
 		    }
-		} else {
+		} else if (canRetrain (g, BUILD_CAMPUS, numDiscipline)) {
 			action isValidRetrain;
 			isValidRetrain.actionCode = RETRAIN_STUDENTS;
-			while (retrainTo < STUDENT_MMONEY) {
-				if (numDiscipline[retrainTo] < 1) {
-					while (retrainFrom < NUM_DISCIPLINES) {
-						if (retrainFrom == STUDENT_BPS || retrainFrom == STUDENT_BQN ||
-							retrainFrom == STUDENT_MJ || retrainFrom == STUDENT_MTV) {
-							if (numDiscipline[retrainFrom] >=
-								getExchangeRate (g, player, retrainFrom, retrainTo)+1) {
-								isValidRetrain.disciplineTo = retrainTo;
-								isValidRetrain.disciplineFrom = retrainFrom;
-							}
-						} else {
-							isValidRetrain.disciplineTo = retrainTo;
-							isValidRetrain.disciplineFrom = retrainFrom;
-						}
-						if (isLegalAction (g, isValidRetrain)) {
-							nextAction = isValidRetrain;
-						}
-						retrainFrom++;
-					}
-				}
-				retrainTo++;
+			isValidRetrain.disciplineTo = determineRetrainTo (BUILD_CAMPUS, numDiscipline);
+			isValidRetrain.disciplineFrom = determineRetrainFrom (g, BUILD_CAMPUS, numDiscipline);
+			if (isLegalAction (g, isValidRetrain)) {
+				nextAction = isValidRetrain;
 			}
 		}
 	} else if (numVacantARC != 0) {
 		printf ("\n---In makeARC()--\n");
 		// make arcs
-		if (isActionCostMet (numDiscipline, OBTAIN_ARC)){
+		if (isActionCostMet (OBTAIN_ARC, numDiscipline)) {
 		    //while loop to try to find possible path to build arc on
 			action isValidARC;
 			isValidARC.actionCode = OBTAIN_ARC;
@@ -170,70 +136,188 @@ action decideAction (Game g) {
 		        	i++;
 				}
 		    }
-		} else {
+		} else if (canRetrain (g, OBTAIN_ARC, numDiscipline)) {
 			action isValidRetrain;
 			isValidRetrain.actionCode = RETRAIN_STUDENTS;
-			while (retrainTo < STUDENT_MJ) {
-				if (numDiscipline[retrainTo] < 1) {
-					while (retrainFrom < NUM_DISCIPLINES) {
-						if (retrainFrom == STUDENT_BPS || retrainFrom == STUDENT_BQN) {
-							if (numDiscipline[retrainFrom] >=
-								getExchangeRate (g, player, retrainFrom, retrainTo)+1) {
-								isValidRetrain.disciplineTo = retrainTo;
-								isValidRetrain.disciplineFrom = retrainFrom;
-							}
-						} else {
-							isValidRetrain.disciplineTo = retrainTo;
-							isValidRetrain.disciplineFrom = retrainFrom;
-						}
-						if (isLegalAction (g, isValidRetrain)) {
-							nextAction = isValidRetrain;
-						}
-						retrainFrom++;
-					}
-				}
-				retrainTo++;
+			isValidRetrain.disciplineTo = determineRetrainTo (OBTAIN_ARC, numDiscipline);
+			isValidRetrain.disciplineFrom = determineRetrainFrom (g, OBTAIN_ARC, numDiscipline);
+			if (isLegalAction (g, isValidRetrain)) {
+				nextAction = isValidRetrain;
 			}
 		}
 	}
 	return nextAction;
 }
 
-int isActionCostMet (int *numDiscipline, int actionCode) {
+int isActionCostMet (int actionCode, int *numDiscipline) {
 
 	int isActionCostMet = FALSE;
 
-	if (actionCode == OBTAIN_ARC && numDiscipline[STUDENT_BPS] >= 1 
+	if (actionCode == OBTAIN_ARC && numDiscipline[STUDENT_BPS] >= 1
 		&& numDiscipline[STUDENT_BQN] >= 1) {
 		isActionCostMet = TRUE;
-	} else if (actionCode == BUILD_CAMPUS && numDiscipline[STUDENT_BPS] >= 1 
+	} else if (actionCode == BUILD_CAMPUS && numDiscipline[STUDENT_BPS] >= 1
 		&& numDiscipline[STUDENT_BQN] >= 1 && numDiscipline[STUDENT_MJ] >= 1
 		&& numDiscipline[STUDENT_MTV] >= 1) {
 		isActionCostMet = TRUE;
-	} else if (actionCode == BUILD_GO8 && numDiscipline[STUDENT_MJ] >= 2 
+	} else if (actionCode == BUILD_GO8 && numDiscipline[STUDENT_MJ] >= 2
 		&& numDiscipline[STUDENT_MMONEY] >= 3) {
 		isActionCostMet = TRUE;
 	} else if (actionCode == START_SPINOFF && numDiscipline[STUDENT_MJ] >= 1
 		&& numDiscipline[STUDENT_MTV] >= 1 && numDiscipline[STUDENT_MMONEY] >= 1) {
 		isActionCostMet = TRUE;
 	}
-	
+
 	return isActionCostMet;
 }
 
-/*action retrainStudents (Game g, int actionCode, int retrainTo, int retrainFrom) {
+int canRetrain (Game g, int actionCode, int *numDiscipline) {
 
+	int canRetrain = FALSE;
 	int player = getWhoseTurn (g);
-	int rate = 0;
 
-	int numDiscipline[NUM_DISCIPLINES];
-	int curDiscipline = 0;
-	while (curDiscipline < NUM_DISCIPLINES) {
-		numDiscipline[curDiscipline] = getStudents (g, player, curDiscipline);
-		curDiscipline++;
+	// exchange rates for retraining based on retraining centres
+	// we are only concerned with disciplineFrom
+	// thus the last argument, disciplineTo is unimportant
+	int rateBPS = getExchangeRate (g, player, STUDENT_BPS, STUDENT_MMONEY);
+	int rateBQN = getExchangeRate (g, player, STUDENT_BQN, STUDENT_MMONEY);
+	int rateMJ = getExchangeRate (g, player, STUDENT_MJ, STUDENT_MMONEY);
+	int rateMTV = getExchangeRate (g, player, STUDENT_MTV, STUDENT_MMONEY);
+	int rateMMONEY = getExchangeRate (g, player, STUDENT_MMONEY, STUDENT_MMONEY);
+
+	int numBPS = numDiscipline[STUDENT_BPS];
+	int numBQN = numDiscipline[STUDENT_BQN];
+	int numMJ = numDiscipline[STUDENT_MJ];
+	int numMTV = numDiscipline[STUDENT_MTV];
+	int numMMONEY = numDiscipline[STUDENT_MMONEY];
+
+	if (actionCode == OBTAIN_ARC) {
+		if (numBPS > rateBPS || numBQN > rateBQN || numMJ >= rateMJ ||
+			numMTV >= rateMTV || numMMONEY >= rateMMONEY) {
+			canRetrain = TRUE;
+		}
+	} else if (actionCode == BUILD_CAMPUS) {
+		if (numBPS > rateBPS || numBQN > rateBQN || numMJ > rateMJ ||
+			numMTV > rateMTV || numMMONEY >= rateMMONEY) {
+			canRetrain = TRUE;
+		}
+	} else if (actionCode == BUILD_GO8)	{
+		if (numBPS >= rateBPS || numBQN >= rateBQN || numMJ > rateMJ+1 ||
+			numMTV >= rateMTV || numMMONEY > rateMMONEY+2) {
+			canRetrain = TRUE;
+		}
+	} else if (actionCode == START_SPINOFF) {
+		if (numBPS >= rateBPS || numBQN >= rateBQN || numMJ > rateMJ ||
+			numMTV > rateMTV || numMMONEY > rateMMONEY) {
+			canRetrain = TRUE;
+		}
 	}
 
-	
+	return canRetrain;
+}
 
-	return retrainStudents;
-}*/
+int determineRetrainTo (int actionCode, int *numDiscipline) {
+
+	int retrainTo = STUDENT_BPS;
+	int retrainTo_MAX = STUDENT_BPS;
+	int curDiscipline = STUDENT_BPS;
+
+	if (actionCode == BUILD_GO8) {
+		if (numDiscipline[STUDENT_MJ] < 2) {
+			retrainTo = STUDENT_MJ;
+		} else if (numDiscipline[STUDENT_MMONEY] < 3) {
+			retrainTo = STUDENT_MMONEY;
+		}
+	} else {
+		if (actionCode == OBTAIN_ARC) {
+			curDiscipline = STUDENT_BPS;
+			retrainTo_MAX = STUDENT_MJ;
+		} else if (actionCode == BUILD_CAMPUS) {
+			curDiscipline = STUDENT_BPS;
+			retrainTo_MAX = STUDENT_MMONEY;
+		} else if (actionCode == START_SPINOFF) {
+			curDiscipline = STUDENT_MJ;
+			retrainTo_MAX = MAX_DISCIPLINE;
+		}
+		while (curDiscipline < retrainTo_MAX) {
+			if (numDiscipline[curDiscipline] < 1) {
+				retrainTo = curDiscipline;
+			}
+			curDiscipline++;
+		}
+	}
+
+	return retrainTo;
+}
+
+int determineRetrainFrom (Game g, int actionCode, int *numDiscipline) {
+
+	int player = getWhoseTurn (g);
+	int retrainFrom = STUDENT_BPS;
+	int curDiscipline = STUDENT_BPS;
+	int rate = 0;
+
+	if (actionCode == OBTAIN_ARC) {
+		while (curDiscipline < MAX_DISCIPLINE) {
+			rate = getExchangeRate (g, player, curDiscipline, STUDENT_MMONEY);
+			if (curDiscipline == STUDENT_BPS || curDiscipline == STUDENT_BQN) {
+				if (numDiscipline[curDiscipline] > rate) {
+					retrainFrom = curDiscipline;
+				}
+			} else {
+				if (numDiscipline[curDiscipline] >= rate) {
+					retrainFrom = curDiscipline;
+				}
+			}
+			curDiscipline++;
+		}
+	} else if (actionCode == BUILD_CAMPUS) {
+		while (curDiscipline < MAX_DISCIPLINE) {
+			rate = getExchangeRate (g, player, curDiscipline, STUDENT_MMONEY);
+			if (curDiscipline == STUDENT_MMONEY) {
+				if (numDiscipline[curDiscipline] >= rate) {
+					retrainFrom = curDiscipline;
+				}
+			} else {
+				if (numDiscipline[curDiscipline] > rate) {
+					retrainFrom = curDiscipline;
+				}
+			}
+			curDiscipline++;
+		}
+	} else if (actionCode == BUILD_GO8) {
+		while (curDiscipline < MAX_DISCIPLINE) {
+			rate = getExchangeRate (g, player, curDiscipline, STUDENT_MMONEY);
+			if (curDiscipline == STUDENT_MJ) {
+				if (numDiscipline[curDiscipline] > rate+2) {
+					retrainFrom = curDiscipline;
+				}
+			} else if (curDiscipline == STUDENT_MMONEY){
+				if (numDiscipline[curDiscipline] > rate+3) {
+					retrainFrom = curDiscipline;
+				}
+			} else {
+				if (numDiscipline[curDiscipline] >= rate) {
+					retrainFrom = curDiscipline;
+				}
+			}
+			curDiscipline++;
+		}
+	} else if (actionCode == START_SPINOFF) {
+		while (curDiscipline < MAX_DISCIPLINE) {
+			rate = getExchangeRate (g, player, curDiscipline, STUDENT_MMONEY);
+			if (curDiscipline == STUDENT_BPS || curDiscipline == STUDENT_BQN) {
+				if (numDiscipline[curDiscipline] >= rate) {
+					retrainFrom = curDiscipline;
+				}
+			} else {
+				if (numDiscipline[curDiscipline] > rate) {
+					retrainFrom = curDiscipline;
+				}
+			}
+			curDiscipline++;
+		}
+	}
+
+	return retrainFrom;
+}
